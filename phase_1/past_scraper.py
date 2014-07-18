6a40e811bfc656a890a9eb6aa1087fadc899f3f8
gregkinman-b48856 <gregory.kinman@freescale.com>
2014-07-18 12:37:22 -0500

    Loaded all current files into the repository.

---
+++ phase_1/past_scraper.py
@@ -0,0 +1,307 @@
+"""
+Greg Kinman - B48856
+Freescale Semiconductor, Inc.
+
+web_scraper.py
+
+Scrapes the web.
+"""
+
+import nltk, csv, pdb
+from urllib import urlopen
+from datetime import datetime
+from bs4 import BeautifulSoup as BS
+from nltk.tokenize.punkt import PunktSentenceTokenizer as ST
+
+GLOBAL_KEYWORDS = [
+                "freescale", "i.mx", "kinetis", "mpu", "mcu", "microcontroller", "microprocessor", "dsp", 
+                "digital signal processor", "analog", "rf", "radio frequency", "sensor", "iot", "internet of things", 
+                "ioe", "internet of everything", "pmic", "ti", "texas instruments", "qualcomm", "broadcom", 
+                "analog devices", "stm", "stmicroelectronics", "atmel", "cavium", "infineon", "intel", "maxell", 
+                "microchip", "soc", "nxp", "nec", "renesas", "riotboard", "riot board"
+]
+
+def main():
+
+    """
+    Driver function. Call whatever functions from below that are necessary.
+    """
+
+    # Gets the text snippets and prepares them for writing to a file.
+    unformatted_element_14_snippets = element_14_scraper()
+    element_14_snippets = []
+    for snippet in unformatted_element_14_snippets:
+        snippet[0].replace(",", "")
+        element_14_snippets.append(snippet)
+
+    # Writes the text and metadata to a CSV file.
+    with open("element_14_snippets.csv", "wb") as f:
+        csv_writer = csv.writer(f)
+        csv_writer.writerow(["Text Snippet", "URL" "Found", "Day", "Month", "Year"])
+        for snippet in element_14_snippets:
+            try:
+                csv_writer.writerow(snippet)
+            except UnicodeEncodeError:
+                print "UnicodeEncodeError thrown. Skipped offending snippet (probably unimportant)."
+                continue
+
+def element_14_scraper():
+
+    """
+    Scrapes element14.com for text about Freescale and its competitors.
+
+    Output:
+    1. element_14_snippets:     list of lists of strs and ints: text snippets with metadata about them
+    """
+
+    # Split URL's to stay within 120 columns.
+    base_url_1 = "http://www.element14.com/community/content?filterID=all~objecttype~objecttype%5Bthread%5D&filterID=al"
+    base_url_2 = "l~language~language%5Bcpl%5D&ICID=menubar_content_discussions&query="
+
+    # Modification for testing purposes.
+    GLOBAL_KEYWORDS = ["freescale"]
+
+    element_14_snippets = []
+    start_index = 0
+    # Searches the online forum using each global keyword (which is probably actually an unrealistic thing to do...).
+    for keyword in GLOBAL_KEYWORDS:
+        at_end = False
+        page_counter = 0
+        # While not on the last search results page:
+        while not at_end:
+            url = base_url_1 + base_url_2 + keyword + "&start=" + str(start_index)
+            if at_element_14_end(url):
+               at_end = True
+            # Gets the URL's for each thread in the page of search results.
+            post_urls = element_14_post_url_getter(url)
+            url_counter = 0
+            for post_url in post_urls:
+                # Grabs text snippets and metadata from the thread.
+                subsnippets = element_14_page_getter(post_url)
+                for snippet in subsnippets:
+                    element_14_snippets.append(snippet)
+                print "URL " + str(url_counter) + " scraped."
+                url_counter += 1
+            start_index += 20
+            page_counter += 1
+            print "\nSearch results page " + str(page_counter) + " scraped.\n"
+
+    # Removes duplicates.
+    element_14_snippets = dict((inner_list[0], inner_list) for inner_list in element_14_snippets).values()
+
+    print "\nElement 14 domain scrape completed. " + str(len(element_14_snippets)) + " snippets found."
+
+    return element_14_snippets
+
+def at_element_14_end(url):
+
+    """
+    Tests whether all the result pages have been scraped already.
+
+    Input:
+    1. url:         str: the URL of the page of search results
+
+    Output:
+    1. end_flag     bool: tells if all the result pages have been scraped already or not
+    """
+
+    end_flag = False
+
+    html = urlopen(url).read()
+    soup_bowl = BS(html)
+    div_tags = soup_bowl.find_all("div")
+
+    # The end page has been hit if there is a div tag in the page source code with class attribute value "j-empty".
+    for div in div_tags:
+        if div.has_attr("class"):
+            if "j-empty" in div["class"]:
+                end_flag = True
+
+    return end_flag
+
+def element_14_post_url_getter(url):
+
+    """
+    Given a search results page URL, returns a list of URL's to the individual forum posts.
+
+    Input:
+    1. url:     str: the URL of the page of search results
+    """
+
+    html = urlopen(url).read()
+    soup = BS(html)
+
+    # All the forum post titles are in td tags with CSS classes "j-td-title."
+    post_urls = []
+    forum_posts = soup.find_all(class_="j-td-title")
+    for post in forum_posts:
+        link_tag = post.find("a")
+        link_url = link_tag["href"]
+        post_urls.append(link_url)
+
+    return post_urls
+
+def element_14_page_getter(url):
+
+    """
+    Gets text snippets for each page in a thread.
+
+    Input:
+    1. url:             str: the url of the first page in the thread
+
+    Output:
+    1. post_snippets:   list of unicode's: the text snippets from the page
+    """
+
+    # Standardizes the URL for looping.
+    new_url_base = url.split("/l/", 1)[0] + "?start="
+    new_url_tail = "&tstart=0"
+
+    at_end = False
+    start = 0
+    post_snippets = []
+    while not at_end:
+        # Checks if the last page has been reached already.
+        page_url = new_url_base + str(start) + new_url_tail
+        html = urlopen(page_url).read()
+        if html.count("jive-rendered-content") < 3:
+            at_end = True
+
+        page_snippets = element_14_snippet_getter(page_url, ["freescale", "i.mx", "kinetis", "mcu", "riot board"])
+
+        for snippet in page_snippets:
+            post_snippets.append(snippet)
+
+        start += 15
+
+    return post_snippets
+
+def element_14_snippet_getter(url, keywords=GLOBAL_KEYWORDS):
+
+    """
+    Given a URL and keywords, returns a list of text snippets including those keywords.
+
+    Inputs:
+    1. url:         str: the desired URL
+    2. keywords:    list of str's: defaults to global_keywords defined in main(); the desired keywords
+
+    Output:
+    1. snippets:    list of unicode's: the desired text snippets
+    """
+
+    # Pulls clean text from the URL, devoid of HTML.
+    try:
+        html = urlopen(url).read()
+    except UnicodeError:
+        print "UnicodeError thrown. Skipped offending URL (probably not in English and thus unanalyzable)."
+        return []
+    soup = BS(html)
+    raw = nltk.clean_html(html)
+
+    # We don't want propaganda written by Freescale employees.
+    if ("FreescaleTools_and_Software" in raw) or ("GregC" in raw) or ("MAb" in raw):
+        return []
+
+    # Finds all HTML subtrees that tell us the date of writing of each post.
+    post_data = soup.find_all(class_="j-post-author")
+
+    # Finds all HTML subtrees comprising the posts themselves.
+    posts = soup.find_all(class_="jive-rendered-content")
+
+    snippets = []
+    tokenizer = ST()
+    dates = []
+    # Assembles the dates of writing of each post.
+    for post_datum in post_data:
+        date_posted = date_getter(post_datum)
+        dates.append(date_posted)
+    # For each post in the page, grabs the text and metadata.
+    for i in range(len(posts)):
+        text = posts[i].get_text()
+        # Splits the text into its individual sentences so that we can pick the ones we like.
+        intermediate_snippets = tokenizer.tokenize(text)
+        # Grabs text containing keywords, as well as sentences preceding and following those with keywords.
+        for j in range(len(intermediate_snippets)):
+            for word in keywords:
+                snippet = intermediate_snippets[j]
+                if (word in snippet.lower()) and ("http" not in snippet.lower()):
+                    offset = 1
+                    for k in range(j, len(intermediate_snippets)):
+                        if word not in intermediate_snippets[k].lower():
+                            break
+                        else:
+                            offset += 1
+                    if j == 0:
+                        subsnippet = intermediate_snippets[j:offset]
+                    else:
+                        subsnippet = intermediate_snippets[j-1:j+offset]
+                    # Puts the individual sentences back together.
+                    token = ""
+                    for sentence in subsnippet:
+                        stripped = sentence.lstrip().rstrip().encode("UTF-8")
+                        token += stripped + " "
+                    try:
+                        if len(token) < 1500:
+                            snippets.append([token, url, dates[i][0], dates[i][1], dates[i][2]])
+                    except IndexError:
+                        continue
+
+    return snippets
+
+def date_getter(soup):
+
+    """
+    Returns a formatted date hidden in nasty unformatted HTML.
+
+    Input:
+    1. soup:        a BeautifulSoup object: the HTML subtree in question
+
+    Output:
+    1. date_posted: tuple: the date the post was written in (d, m, y) format
+    """
+
+    # Removes an unwanted span subtree from the mother subtree, since all it is is junk.
+    unwanted = soup.find_all(class_="j-thread-replyto")
+    if unwanted:
+        unwanted[0].extract()
+
+    # Extracts the author. Accounts for varying edge cases (boy, are there edge cases in Jive's code).
+    author = soup.find_all("a")
+    if author:
+        author = author[0].get_text()
+    else:
+        author = soup.find_all("strong")[0].get_text()
+
+    # Cleans up the date string extracted.
+    date_posted = date_string_cleaner(soup, author)
+
+    return date_posted
+
+def date_string_cleaner(soup, author):
+
+    """
+    Formats the unformatted date string into a nice tuple.
+
+    Inputs:
+    1. soup:    a BeautifulSoup object: the HTML subtree in questions
+    2. author:  unicode: the author of the post, which is irrelevant for Element14, and thus needs to be removed
+    """
+
+    # Cleans it up.
+    date_and_author = soup.get_text()
+    date_posted_unformatted = date_and_author.replace(author, "")
+    date_posted_unformatted = date_posted_unformatted.replace(",", "")
+    
+    # Uses the datetime module to format the date nicely into a tuple.
+    try:
+        datetime_object = datetime.strptime(date_posted_unformatted.lstrip().rstrip(), "%b %d %Y %I:%M %p")
+    except ValueError:
+        # This error shouldn't ever be thrown anymore. But, if it does, we want to know why the hell why.
+        pdb.set_trace()
+    date_posted = (datetime_object.day, datetime_object.month, datetime_object.year)
+
+    return date_posted
+
+# This is necessary because we only want this script called explicitly from the command line (for now).
+if __name__ == "__main__":
+    main()
---
+++ phase_2/snippet_labeler.py
@@ -0,0 +1,135 @@
+"""
+Greg Kinman - B48856
+Freescale Semiconductor, Inc.
+
+snippet_labeler.py
+
+Labels the text snippets with their sentiment values.
+"""
+
+import csv, subprocess, pdb, os
+from bs4 import BeautifulSoup as BS
+from nltk.tokenize.punkt import PunktSentenceTokenizer as ST
+
+def main():
+
+    """
+    Operates on a .csv file in the current directory that contains sentences with commas removed, as well as other 
+    labeled data items about those sentences. Creates a new .csv file that has the sentiment values appended to the 
+    original data (i.e., in a new rightmost column).
+    """
+
+    # Gets the input and output filenames from the user and asks to proceed.
+    print
+    print "Please enter the name of the input .csv file:"
+    print
+    input_filename = raw_input()
+    print
+    print "Okay, now please enter the desired output .csv filename:"
+    print
+    output_filename = raw_input()
+    print
+    print "Thanks! The file will be passed to the CoreNLP software."
+    print "The output .csv file will be placed in the current directory."
+    print
+    print "There's about to be a lot of console output; ignore it. Type c and hit Enter to continue."
+    print
+    pdb.set_trace()
+
+    # Gets just the text snippets from the input .csv file and puts them into .txt files to be passed to CoreNLP.
+    filenames = csv_to_txt(input_filename)
+
+    # Runs CoreNLP.
+    subprocess.call("run_core_nlp.cmd")
+
+    # Parses NLP's .xml output to label the sentences.
+    xml_to_csv(filenames, input_filename, output_filename)
+
+def csv_to_txt(input_filename):
+
+    """
+    Turns the snippets in the .csv file into a form that's easily passable to CoreNLP.
+
+    Input:
+    1. input_filename:      str: the name of the .csv file that contains the text data.
+
+    Output:
+    1. filenames:           list of strs: the paths to the files containing the text snippets
+    """
+
+    # Pulls the text snippets out of the .csv file into a data structure.
+    snippets = []
+    with open(input_filename, "rb") as f:
+        file_reader = csv.reader(f)
+        tokenizer = ST()
+        for row in file_reader:
+            subsnippet = row[0]
+            subsnippets = tokenizer.tokenize(subsnippet)
+            snippets.append(subsnippets)
+
+    # Makes a dump folder.
+    subdirectory = "snippet_files"
+    try:
+        os.mkdir(subdirectory)
+    except Exception:
+        pass
+
+    # Dumps the text to files in the dump folder.
+    counter = 1
+    filenames = []
+    for snippet in snippets:
+        filename = "snippet_file_" + str(counter) + ".txt"
+        filepath = os.path.join(subdirectory, filename)
+        filenames.append(filepath)
+        with open(filepath, "wb") as f:
+            for sentence in snippet:
+                f.write(sentence + "\n")
+        counter += 1
+
+    # Assembles the list of filepaths so CoreNLP knows where to look.
+    with open("filenames.txt", "wb") as f:
+        for filepath in filenames:
+            f.write(filepath + "\n")
+
+    return filenames
+
+def xml_to_csv(filenames, input_filename, output_filename):
+
+    """
+    Writes the output .csv file given the labeled sentences.
+
+
+    """
+
+    # Parses the .xml files to get all the sentiment values into a list.
+    sentiment_values = []
+    for filepath in filenames:
+        soup = BS(open(filepath + ".xml", "rb"), "xml")
+        xml_sentences = soup.find_all("sentence")
+        intermediate_values = []
+        for sentence in xml_sentences:
+            intermediate_values.append(sentence["sentimentValue"].encode("ascii"))
+        concatenated_values = ""
+        for value in intermediate_values:
+            concatenated_values += value
+        sentiment_values.append(concatenated_values)
+    sentiment_values = sentiment_values[1:]
+
+    # Creates the output .csv file, appending the sentiment values to it with a new rightmost column.
+    with open(input_filename, "rb") as f:
+        with open(output_filename, "wb") as g:
+            input_reader = csv.reader(f)
+            output_writer = csv.writer(g)
+            # Adds the new column label to the end of the first row.
+            full_file = []
+            row = next(input_reader)
+            row.append("Sentiment Value")
+            full_file.append(row)
+            # Adds the sentiment values to the rightmost column.
+            for row, value in zip(input_reader, sentiment_values):
+                row.append(value)
+                full_file.append(row)
+            output_writer.writerows(full_file)
+
+if __name__ == "__main__":
+    main()
