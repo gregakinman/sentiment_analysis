a3bd7efd07df53b615580bec6b2f709e4796358f
Greg Kinman - B48856 <B48856@freescale.com>
2014-07-24 14:22:00 -0500

    Finished snippet grouper documentation.

--- phase_2/snippet_grouper.py
+++ phase_2/snippet_grouper.py
@@ -2,38 +2,62 @@
 Greg Kinman - B48856
 Freescale Semiconductor, Inc.
 
-.py
+snippet_grouper.py
 
-
+Groups snippets by sentiment score for group analysis.
 """
 
-import csv
+import csv, pdb
 from nltk.tokenize.punkt import PunktSentenceTokenizer as ST
 
-csv_filename = raw_input()
+def main():
 
-groups = {0: [], 1: [], 2: [], 3: [], 4: []}
+    """
+    Main function.
+    """
 
-with open(csv_filename, "rb") as f:
-    csv_reader = csv.reader(f)
-    tokenizer = ST()
-    counter = 0
-    for row in csv_reader:
-        if counter == 0:
-            counter += 1
-            continue
-        sentiments = str(row[-1])
-        snippets = tokenizer.tokenize(row[0])
-        for i in range(len(sentiments)):
-            groups[int(sentiments[i])].append(snippets[i])
+    print "Please name the .csv file in which the text data resides:"
+    csv_filename = raw_input()
+    print "Thanks! The text will be placed, sentence by sentence, into filenames of form value_#.csv. Hit 'c' and enter/return to continue."
+    pdb.set_trace()
 
-score_classes = {}
-for group in groups:
-    if groups[group]:
-        score_classes[group] = groups[group]
+    grouper(csv_filename)
 
-for score in score_classes:
-    with open("value_" + str(score) + ".csv", "wb") as f:
-        writer = csv.writer(f)
-        for snippet in score_classes[score]:
-            writer.writerow([snippet])
+def grouper(csv_filename):
+
+    """
+    Groups text snippets into their sentiment score classes.
+
+    Input:
+    1. csv_filename:    str: the name of the .csv file in which the text data resides.
+    """
+
+    groups = {0: [], 1: [], 2: [], 3: [], 4: []}
+
+    with open(csv_filename, "rb") as f:
+        csv_reader = csv.reader(f)
+        tokenizer = ST()
+        counter = 0
+        # Tokenizes each row and splits up the tokens into groups.
+        for row in csv_reader:
+            if counter == 0:
+                counter += 1
+                continue
+            sentiments = str(row[-1])
+            snippets = tokenizer.tokenize(row[0])
+            for i in range(len(sentiments)):
+                groups[int(sentiments[i])].append(snippets[i])
+
+    # Writes the new groups to their own separate groups.
+    score_classes = {}
+    for group in groups:
+        if groups[group]:
+            score_classes[group] = groups[group]
+    for score in score_classes:
+        with open("value_" + str(score) + ".csv", "wb") as f:
+            writer = csv.writer(f)
+            for snippet in score_classes[score]:
+                writer.writerow([snippet])
+
+if __name__ == "__main__":
+    main()
